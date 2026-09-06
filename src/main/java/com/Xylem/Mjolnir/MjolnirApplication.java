32b260dc80269ad2b1a143f2117c523425bd5354
Yugan <yugantkumaran@gmail.com>
2026-09-06 15:40:47 +0530

    Initial Project Skeleton

---
+++ src/main/java/com/Xylem/Mjolnir/MjolnirApplication.java
@@ -0,0 +1,13 @@
+package com.Xylem.Mjolnir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MjolnirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MjolnirApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Xylem/Mjolnir/MjolnirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Xylem.Mjolnir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MjolnirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
